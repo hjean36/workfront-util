e0e0913a6499332c20e0de22e5a9e8f4c99b14aa
hjean36 <hjean36@outlook.com>
2019-08-22 12:30:24 -0400

    Add unit test

---
+++ tests/test_db_connection.py
@@ -0,0 +1,50 @@
+import unittest 
+import asyncio 
+from pathlib import Path
+import glob
+import datetime
+import os
+import pyodbc
+
+
+#Third Party Imports 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String,  create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+import pandas as pd
+
+#Local Imports 
+from services.workfront import WorkfrontAPI 
+from services.settings import ENVSettings, DatabaseConfig, Defaults
+from services.db_util import DBU
+
+class TestDatabase(unittest.TestCase): 
+
+    def setUp(self): 
+        #TODO: Create test db 
+        #TODO:  Create Test Table 
+        #TODO; Create test values
+        pass 
+
+    def tearDown(self): 
+        #Delete Test Tables 
+        pass 
+
+    def test_connect_to_sql_database(self): 
+            #TODO: Import test values
+            server = DatabaseConfig().server
+            database = DatabaseConfig().database
+            username = DatabaseConfig().username
+            password =   DatabaseConfig().password
+            cnxn = pyodbc.connect('driver={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+            cursor = cnxn.cursor()
+
+            cursor.execute("SELECT * FROM [dbo].[test_table]") 
+            row = cursor.fetchone() 
+            while row: 
+                print(row[0])
+                row = cursor.fetchone()
+
+
+if __name__ == "__main__":
+    unittest.main() 
